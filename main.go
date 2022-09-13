25f0a8dd2396c78e7dddcaea95f252f1146ec1ca
Cyril Dumange <cyril.dumange@adevinta.com>
2022-09-13 08:21:36 +0200

    refacto: move to fyne

--- main.go
+++ main.go
@@ -1,44 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/cdumange/twitchscan/ui"
-	"github.com/maxence-charriere/go-app/v9/pkg/app"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
+	a := app.New()
+	w := a.NewWindow("Hello World !")
 
-	server := http.Server{
-		Addr:    fmt.Sprintf(":%d", 8000),
-		Handler: nil,
-	}
-
-	go func(ctx context.Context) {
-		<-ctx.Done()
-		server.Shutdown(context.Background())
-	}(ctx)
-
-	app.Route("/", new(ui.Front))
-	app.RunWhenOnBrowser()
-
-	http.Handle("/", &app.Handler{
-		Name:        "test",
-		Description: "first test",
-		Styles: []string{
-			"/web/css/global.css",
-		},
-	})
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Println(err)
-	}
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
